fc3ef2bbcb7bcf9a35c33d7382c5c0cae3a56dd2
Ashok Siyani <ashok.siyani@gmail.com>
2017-01-14 11:52:54 +0000

    add get /todo/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 const {myMongoose} = require('./db/mongoose');
@@ -25,6 +26,24 @@ app.get('/todos', (req,res) => {
     
     Todo.find().then( (todos) => res.send({todos}))
                .catch( err => res.status(400).send(e));
+});
+
+app.get('/todos/:id' , (req,res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send(); 
+    }
+
+    Todo.findById(id).then( (result) => {
+                    if(!result)
+                        return res.status(404).send();
+                    res.send({todo:result});
+                })
+                .catch( (err) => res.status(400).send(err));
+
+
+
 });
 
 
