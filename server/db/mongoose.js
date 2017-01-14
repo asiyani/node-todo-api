6e40044f6269e81af210cbab69182746b9f0ac73
Ashok Siyani <ashok.siyani@gmail.com>
2017-01-14 12:41:08 +0000

    ready for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,8 @@ const myMongoose = require('mongoose');
 
 
 myMongoose.Promise = global.Promise;
-myMongoose.connect('mongodb://localhost:27017/TodoApp');
+//myMongoose.connect('mongodb://localhost:27017/TodoApp');
+myMongoose.connect('mongodb://test:test@ds141128.mlab.com:41128/node-todo-app');
+
 
 module.exports = {myMongoose};
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {myMongoose} = require('./db/mongoose');
 const {Todo}  = require('./models/todo');
 const {User} = require('./models/user');
 
-
+let port = process.env.PORT || 3000;
 
 let app = express();
 app.use(bodyParser.json()); 
@@ -56,7 +56,7 @@ app.get('/todos/:id' , (req,res) => {
 
 
 
-app.listen(3000, () => console.log('Started on port 3000'));
+app.listen(port, () => console.log('Started on port '+port));
 
 
 module.exports = {app};
